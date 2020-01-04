7d1ce4d5d1ca55401d141cafd9a3a41cbe3abec5
Nitish <sharma.nitish@live.com>
2020-01-04 14:42:42 -0500

    async methods

--- src/api/soap/stock.ts
+++ src/api/soap/stock.ts
@@ -4,27 +4,44 @@ import * as soap from "soap";
 export class Stock
 {
     private readonly _url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
-    
-    
+
+
     public constructor()
     {
-        
+
     }
-    
-    
+
+
     public getStock(): void
     {
         // @ts-ignore
         const args = {
             sCountryISOCode: "IN"
         };
-        
-        soap.createClient(this._url, function (_err, client)
+
+        // soap.createClient(this._url, function (_err, client)
+        // {
+        //     client.CountryInfoService.CountryInfoServiceSoap.CapitalCity(args, function (_err: any, result: any)
+        //     {
+        //         console.log(result);
+        //     });
+        // });
+
+        // soap.createClientAsync(url).then((client) =>
+        // {
+        //     return client.MyFunctionAsync(args);
+        // }).then((result) =>
+        // {
+        //     console.log(result);
+        // });
+
+        soap.createClientAsync(this._url).then((client) =>
         {
-            client.CountryInfoService.CountryInfoServiceSoap.CapitalCity(args, function (_err: any, result: any)
-            {
-                console.log(result);
-            });
+            // return client.describe();
+            return client.CountryInfoService.CountryInfoServiceSoap.CapitalCity(args);
+        }).then((result: any) => 
+        {
+            console.log(result);
         });
     }
 }
